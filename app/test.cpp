e210d076e26ef0fd212bf6051f71ca1f11a04569
tesonkim <teson0228@gmail.com>
2024-04-16 00:55:47 +0900

    aaa

--- app/test.cpp
+++ app/test.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+using namespace std;
+#include <stdio.h>
 
 int main() {
-    std::cout << "hello world!" << std::endl;
+    cout << "ストリームを使った出力" << endl;
+    printf("関数を使った出力\n");
     return 0;
 }
